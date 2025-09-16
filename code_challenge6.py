c8708ca02e453a96fc2d8a6b74575699da0d11d1
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-09-16 13:34:36 +0800

    Add files via upload

---
+++ code_challenge6.py
@@ -0,0 +1,8 @@
+sum = 0
+for x in range(10):#jbs1
+    number = eval(input("Enter any number: "))
+    if number % 2:
+        sum += number
+print("The summation of the odd numbers is:", sum)
+
+#hehehehe
