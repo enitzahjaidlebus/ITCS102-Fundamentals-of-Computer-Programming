27cb8e481e7dfcd23777e34c0cef9bf9bbfa5e31
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-09-17 14:28:32 +0800

    Add files via upload

---
+++ code_challenge7.py
@@ -0,0 +1,7 @@
+print("⏳ COUNTDOWN TIMER SIMULATOR")
+countdown_number = eval(input("Enter the starting number for countdown: "))
+print("Countdown Started:")
+for liftoff in range (countdown_number,0,-1):
+    print(liftoff,)
+
+print("Liftoff!")
