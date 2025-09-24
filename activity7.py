a6c737e24b2888f72cd24a8d30a38356c0019a2f
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-09-24 11:28:58 +0800

    Add files via upload

---
+++ activity7.py
@@ -0,0 +1,13 @@
+a = 22
+print("Your number is", a)
+
+a -= 5
+print("The result is", a)
+
+a -= 14
+a *= 2 
+a += 30
+a /= 2
+a += 3
+
+print("The result is", a)
---
+++ activity8.py
@@ -0,0 +1,11 @@
+a = 11
+b = 3
+c = 5
+d = 3
+
+n1 = "Jhaztine"
+n2 = "Jhaztine"
+
+print( a < b ) 
+print( c >= d )
+print( n1 == n2 )
