89f28c6253d8857b4ab7b5e920d1a7f5d4a005f6
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-09-24 11:32:23 +0800

    Add files via upload

---
+++ activity9.py
@@ -0,0 +1,11 @@
+a = 2
+b = 2
+c = 11
+d = 58
+
+username = "Jhaztine"
+password = "pogiako123"
+print((username == "Jhaztine") and (password == "pogiako123"))
+print((a > b) and (c < d))
+print((a > b) or (c < d)) 
+print(not((a > b) or (c < d)))
