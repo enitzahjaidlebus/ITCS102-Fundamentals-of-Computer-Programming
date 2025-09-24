19739c36cc06c7ef566825ded99ce56ff786595a
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-09-24 11:37:31 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,10 @@
+username = "Jhaztine"
+password = "pogiako123"
+
+user = input("Enter your username: ")
+passwrd = input("Enter your password: ")
+
+if (user == username) and (passwrd == password):
+    print("Access Granted, Welcome", username,"!")
+else:
+    print("Access Denied, Please try again")
