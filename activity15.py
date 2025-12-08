0a39b9a9a980dddb40f96d05cdbfb0a858524d9f
Jhaztine Subeldia <subeldiajhaztine@gmail.com>
2025-12-08 16:19:19 +0800

    Add files via upload

---
+++ activity15.py
@@ -0,0 +1,13 @@
+fname = 'jhaztine'
+lname = 'subeldia'
+print(f"My full name is {fname} {lname}")
+
+fname = 'jhaztine'
+lname = 'subeldia'
+print(f"My full name is {fname.upper()} {lname.upper()}")
+
+sum = 0 
+for i in range(1, 6):
+    x  = eval(input(f"{i} - Input a number --> "))
+    sum += x 
+print(f"The total sum is {sum}")
